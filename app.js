9beb062e455e6fc6e3b03835a5fba5f8e6798e7d
San-Moshe <sanmoshe100@gmail.com>
2018-02-26 23:35:57 +0200

    app.js Updates

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var jsonMessage = 'No Data';
+var image;
+var rawJsonData;
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.get('/image', function(req, res) {
+    console.log('ahhhhhh!!!!!');
+    res.send(image);
+  });
+app.get('/arduino', function(req, res) {
+    console.log('ahhhhhh!!!!!');
+    res.send(rawJsonData);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +54,55 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//UDP Socket
+var udp = require('dgram');
+
+// --------------------creating a udp server --------------------
+
+// creating a udp server
+var server = udp.createSocket('udp4');
+
+// emits when any error occurs
+server.on('error',function(error){
+  console.log('Error: ' + error);
+  server.close();
+});
+
+// emits on new datagram msg
+server.on('message',function(msg,info){
+  //console.log('Data received from client : ' + msg.toString());
+  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+  jsonMessage = msg;
+  
+  if(jsonMessage.type == 'Image') {
+      
+  }
+  else if(jsonMessage.type == 'Arduino') {
+
+  }
+
+  fs.writeFile('image.jpeg', jsonMessage, function(err) {
+    if(err) throw err;
+  });
+});
+
+//emits when socket is ready and listening for datagram msgs
+server.on('listening',function(){
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log('Server is listening at port' + port);
+  console.log('Server ip :' + ipaddr);
+  console.log('Server is IP4/IP6 : ' + family);
+});
+
+//emits after the socket is closed using socket.close();
+server.on('close',function(){
+  console.log('Socket is closed !');
+});
+
+server.bind(2222);
 
 module.exports = app;
