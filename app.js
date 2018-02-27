2b2c5335efc088cdbf54655cd04a242aca8c90f9
San-Moshe <sanmoshe100@gmail.com>
2018-02-27 05:05:23 +0200

    Original multiple inputs

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ var users = require('./routes/users');
 //var AsyncLock = require('async-lock');
 var fs = require('fs');
 //var lock = new AsyncLock();
-
+var imageAddr;
+var ardunioAddr;
 var jsonMessage = 'No Data';
 var image;
 var rawJsonData;
+var blackBoxesImageDict = []; 
+var blackBoxesArduinoDict = [];
 var app = express();
 
 // view engine setup
@@ -29,16 +32,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
-app.get('/image', function(req, res) {
-    console.log('Image sent!!!!!');
-    res.send(image);
-  });
-app.get('/arduino', function(req, res) {
-    console.log('arduino data sent!!!!!', rawJsonData);
-    res.send(rawJsonData);
+ 
+app.get('/IDS', function(req, res) {
+  var idKeys = Object.keys(blackBoxesImageDict);
+  var current;
+  var jsonItem = [];
+    idKeys.forEach(function(Id) 
+    {
+      current = {
+        id: Id.toString(),
+        name: 'Input ' + Id.toString()
+      };
+      jsonItem.push(current);
+    });
+    var ret = {
+      availableOptions: jsonItem,
+      selectedOption: current,
+    }
+  res.send(ret);
 });
 
+app.get('/image/:imageId', function(req, res) {
+    console.log('Image sent!!!!!');
+    res.send(blackBoxesImageDict[req.params.imageId]);
+  });
+app.get('/arduino/:arduinoId', function(req, res) {
+    console.log('arduino data sent!!!!!', rawJsonData);
+    res.send(blackBoxesArduinoDict[req.params.arduinoId]);
+});
+
+app.get('/image', function(req, res) {
+  // console.log('Image sent!!!!!');
+  // res.send(image);
+});
+app.get('/arduino', function(req, res) {
+  // console.log('arduino data sent!!!!!', rawJsonData);
+  // res.send(rawJsonData);
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -83,18 +113,21 @@ server.on('message',function(msg,info){
 
   if(jsonMessage.Type === 'Image') {
     console.log("accessed!");
+      imageAddr = '/image' + jsonMessage.Id;
       image64 = jsonMessage.Bitmap;
-      var imageBuffer = Buffer.from(image64, 'base64');
-      fs.writeFile('mynewfile3.jpeg', imageBuffer, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
+      // var imageBuffer = Buffer.from(image64, 'base64');
+      // fs.writeFile('mynewfile3.jpeg', imageBuffer, function (err) {
+      //   if (err) throw err;
+      //   console.log('Saved!');
+      // });
       image= image64;
+      blackBoxesImageDict[jsonMessage.Id] = image64;
   }
   else if(jsonMessage.Type === 'Arduino') {
-
+    ardunioAddr = '/arduino' + jsonMessage.Id;
     rawJsonData = jsonMessage;
     //console.log('initial', rawJsonData)
+    blackBoxesArduinoDict[jsonMessage.Id] = rawJsonData;
   }
 });
 
@@ -104,14 +137,14 @@ server.on('listening',function(){
   var port = address.port;
   var family = address.family;
   var ipaddr = address.address;
-  console.log('Server is listening at port' + port);
-  console.log('Server ip :' + ipaddr);
-  console.log('Server is IP4/IP6 : ' + family);
+  console.log('Server is listening at port: ' + port);
+  console.log('Server IP: ' + ipaddr);
+  console.log('Server is IP4/IP6: ' + family);
 });
 
 //emits after the socket is closed using socket.close();
 server.on('close',function(){
-  console.log('Socket is closed !');
+  console.log('Socket is closed! ');
 });
 
 server.bind(2222);
